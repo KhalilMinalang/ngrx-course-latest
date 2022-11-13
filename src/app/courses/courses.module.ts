23e9ce294485923181b157033a32a66ea3f68805
KhalilMinalang <minalang.khalil@gmail.com>
2022-11-13 13:24:08 +0800

    Completed: 40 Querying Store Data with NgRx Data and the entities Observable

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -52,7 +52,8 @@ export const coursesRoutes: Routes = [
 
 const entityMetadata: EntityMetadataMap = {
   Course: {
-    //
+    // sort courses data
+    sortComparer: compareCourses,
   },
 };
 
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,80 +1,66 @@
-import {Component, OnInit} from '@angular/core';
-import {compareCourses, Course} from '../model/course';
-import {Observable} from "rxjs";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
-import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import {map, shareReplay} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { compareCourses, Course } from "../model/course";
+import { Observable } from "rxjs";
+import { defaultDialogConfig } from "../shared/default-dialog-config";
+import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { map, shareReplay } from "rxjs/operators";
+import { CoursesHttpService } from "../services/courses-http.service";
+import { CourseEntityService } from "../services/course-entity.service";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  promoTotal$: Observable<number>;
 
-    promoTotal$: Observable<number>;
+  loading$: Observable<boolean>;
 
-    loading$: Observable<boolean>;
+  beginnerCourses$: Observable<Course[]>;
 
-    beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
-    advancedCourses$: Observable<Course[]>;
+  constructor(
+    private dialog: MatDialog,
+    private coursesService: CourseEntityService
+  ) {}
 
-
-    constructor(
-      private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
-
-    }
-
-    ngOnInit() {
-      this.reload();
-    }
+  ngOnInit() {
+    this.reload();
+  }
 
   reload() {
-
-    const courses$ = this.coursesHttpService.findAllCourses()
+    this.beginnerCourses$ = this.coursesService.entities$
+      //
       .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
+        map((courses) =>
+          courses.filter((course) => course.category == "BEGINNER")
+        )
       );
 
-    this.loading$ = courses$.pipe(map(courses => !!courses));
-
-    this.beginnerCourses$ = courses$
+    this.advancedCourses$ = this.coursesService.entities$
+      //
       .pipe(
-        map(courses => courses.filter(course => course.category == 'BEGINNER'))
+        map((courses) =>
+          courses.filter((course) => course.category == "ADVANCED")
+        )
       );
 
-
-    this.advancedCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'ADVANCED'))
-      );
-
-    this.promoTotal$ = courses$
-        .pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
-
+    this.promoTotal$ = this.coursesService.entities$
+      //
+      .pipe(map((courses) => courses.filter((course) => course.promo).length));
   }
 
   onAddCourse() {
-
     const dialogConfig = defaultDialogConfig();
 
     dialogConfig.data = {
-      dialogTitle:"Create Course",
-      mode: 'create'
+      dialogTitle: "Create Course",
+      mode: "create",
     };
 
     this.dialog.open(EditCourseDialogComponent, dialogConfig);
-
   }
-
-
 }
